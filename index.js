cdba3ba1a01c96f7bdc55b3e0e6ead5e5f48ba4a
Lucas Gondin <lucasgondin@hotmail.com>
2020-07-07 16:09:50 -0300

    alteração da function de preço

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const Lucas = ''
 const infoPotato = {
     origem: 'mg',
     destino: 'sp',
-    preco: 39.90,
+    preco: 8.90,
     comprador: 'fox',
 }
 
 if (infoPotato.preco === 39.90) {
     console.log('O preço está caro')
-} else if (infoPotato.preco === 12.90) {
+} else if (infoPotato.preco <= 12.90) {
     console.log('o preço está baixo')
 } else {
     console.log('o preço está incorreto')
